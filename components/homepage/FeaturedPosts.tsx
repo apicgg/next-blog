8c1d27f511f1d47591ca83cdb9039b7dce9cef6f
Anurag Pramanik <p.anurag@outlook.com>
2023-04-12 22:24:30 +0530

    Add slug route for posts

--- components/homepage/FeaturedPosts.tsx
+++ components/homepage/FeaturedPosts.tsx
@@ -1,5 +1,13 @@
-const FeaturedPosts = () => {
-  return <div>FeaturedPosts</div>;
+import PostGrid from "../posts/PostGrid";
+import classes from "./FeaturedPosts.module.css";
+
+const FeaturedPosts = (props) => {
+  return (
+    <section className={classes.latest}>
+      <h2>Featured Posts</h2>
+      <PostGrid posts={props.posts} />
+    </section>
+  );
 };
 
 export default FeaturedPosts;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,42 @@
-import FeaturedPosts from '../components/homepage/FeaturedPosts';
-import Hero from '../components/homepage/Hero';
+import FeaturedPosts from "../components/homepage/FeaturedPosts";
+import Hero from "../components/homepage/Hero";
+
+const DUMMY_POSTS = [
+  {
+    slug: "getting-started-with-nextjs",
+    title: "Getting started with Nextjs",
+    image: "getting-started-nextjs.png",
+    excerpt: "NextJS",
+    date: "2023-04-12",
+  },
+  {
+    slug: "getting-started-with-nextjs2",
+    title: "Getting started with Nextjs",
+    image: "getting-started-nextjs.png",
+    excerpt: "NextJS",
+    date: "2023-04-12",
+  },
+  {
+    slug: "getting-started-with-nextjs3",
+    title: "Getting started with Nextjs",
+    image: "getting-started-nextjs.png",
+    excerpt: "NextJS",
+    date: "2023-04-12",
+  },
+  {
+    slug: "getting-started-with-nextjs4",
+    title: "Getting started with Nextjs",
+    image: "getting-started-nextjs.png",
+    excerpt: "NextJS",
+    date: "2023-04-12",
+  },
+];
 
 export default function HomePage() {
   return (
     <>
       <Hero />
-      <FeaturedPosts />
+      <FeaturedPosts posts={DUMMY_POSTS} />
     </>
   );
 }
