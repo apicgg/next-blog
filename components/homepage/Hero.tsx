9c14aa091da3a3d8cac5fb01f503a4df749c9fb6
Anurag Pramanik <p.anurag@outlook.com>
2023-04-12 22:27:49 +0530

    Adjust the image on hero component

--- components/homepage/Hero.tsx
+++ components/homepage/Hero.tsx
@@ -1,15 +1,15 @@
-import Image from 'next/image';
-import classes from './Hero.module.css';
+import Image from "next/image";
+import classes from "./Hero.module.css";
 
 function Hero() {
   return (
     <section className={classes.hero}>
       <div className={classes.image}>
         <Image
-          src='https://avatars.githubusercontent.com/u/78271602?s=400&u=1ec23d8138196ff9720c8db66117832bfcdc9e10&v=4'
-          alt='Anurag Pramanik Avatar'
-          width={300}
-          height={300}
+          src="https://avatars.githubusercontent.com/u/78271602?s=400&u=1ec23d8138196ff9720c8db66117832bfcdc9e10&v=4"
+          alt="Anurag Pramanik Avatar"
+          width={200}
+          height={200}
         />
       </div>
       <h1>Hi, I&apos;m Anurag</h1>
