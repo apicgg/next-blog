a506fdb3309f3f74c3ada2d9ef7466b976748406
Anurag Pramanik <p.anurag@outlook.com>
2023-04-15 15:27:19 +0530

    revert(post): add PostContent.tsx

--- components/posts/PostDetail/PostContent.tsx
+++ components/posts/PostDetail/PostContent.tsx
@@ -1,5 +1,7 @@
 import Image from "next/image";
 import ReactMarkdown from "react-markdown";
+import { Prism } from "react-syntax-highlighter";
+import { atomDark } from "react-syntax-highlighter/dist/cjs/styles/prism";
 import classes from "./PostContent.module.css";
 import PostHeader from "./PostHeader";
 
@@ -17,15 +19,38 @@ const PostContent = ({ post }: PostContentProps) => {
   const imagePath = `/images/posts/${post.slug}/${post.image}`;
 
   const customRenderers = {
-    image(image: { src: string; alt: string }) {
-      return (
-        <Image
-          src={`/images/posts/${post.slug}/${image.src}`}
-          alt={image.alt}
-          width={600}
-          height={300}
-        />
-      );
+    // image(image: { src: string; alt: string }) {
+    //   return (
+    //     <Image
+    //       src={`/images/posts/${post.slug}/${image.src}`}
+    //       alt={image.alt}
+    //       width={600}
+    //       height={300}
+    //     />
+    //   );
+    // },
+    paragraph(paragraph: { node: any; children: React.ReactNode }) {
+      const { node, children } = paragraph;
+
+      if (node.children[0].type === "image") {
+        const image = node.children[0];
+        return (
+          <div className={classes.image}>
+            <Image
+              src={`/images/posts/${post.slug}/${image.url}`}
+              alt={image.alt}
+              width={600}
+              height={300}
+            />
+          </div>
+        );
+      }
+      return <p>{children}</p>;
+    },
+
+    code(code: { language?: string | undefined; value: string | string[] }) {
+      const { language, value } = code;
+      return <Prism language={language} children={value} style={atomDark} />;
     },
   };
 
@@ -36,4 +61,5 @@ const PostContent = ({ post }: PostContentProps) => {
     </article>
   );
 };
+
 export default PostContent;
